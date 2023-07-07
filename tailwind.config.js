b8907bb5dae9fd75f429ddb4068605b44e318437
Somraj Mukherjee <somraj.mukherjee@yocket.in>
2023-07-07 21:11:13 +0530

    👌 IMPROVE: added font faces

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      'primary-regular': ['Ubuntu-Regular', 'sans-serif'],
+      'primary-bold': ['Ubuntu-Bold', 'sans-serif'],
+      'primary-medium': ['Ubuntu-Medium', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
