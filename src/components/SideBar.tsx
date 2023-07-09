89f003918cff6efb6de8a718da1032638a0bbadb
Somraj Mukherjee <somraj.mukherjee@yocket.in>
2023-07-09 23:20:53 +0530

    👌 IMPROVE: added sidebar image

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,4 @@
+import SideBarImage from '../../src/assets/images/bg-sidebar-desktop.svg';
 import { SIDE_BAR_MENU_OPTIONS } from '../constants';
 import { StepType } from '../features/payment/paymentSlice';
 import NavItem from './NavItem';
@@ -8,7 +9,12 @@ interface SideBarProps {
 
 const SideBar = ({ currentStep }: SideBarProps) => {
   return (
-    <aside className="-mx-10 flex h-[30vh] items-start justify-center bg-[url('../../src/assets/images/bg-sidebar-desktop.svg')] bg-cover bg-bottom pt-6 desktop:m-3 desktop:h-auto desktop:flex-col desktop:justify-start desktop:gap-6 desktop:rounded-lg desktop:bg-center desktop:py-8">
+    <aside
+      className="-mx-10 flex h-[30vh] items-start justify-center bg-cover bg-bottom pt-6 desktop:m-3 desktop:h-auto desktop:flex-col desktop:justify-start desktop:gap-6 desktop:rounded-lg desktop:bg-center desktop:py-8"
+      style={{
+        backgroundImage: `url(${SideBarImage})`,
+      }}
+    >
       {/* SIDE BAR MENU ITEM */}
       {SIDE_BAR_MENU_OPTIONS.map((menuItem) => (
         <NavItem key={menuItem.step} menuItem={menuItem} currentStep={currentStep} />
