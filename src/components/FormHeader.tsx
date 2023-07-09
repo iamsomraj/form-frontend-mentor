25c2c5282591199f0c82c363e5f22811ebe96ae2
Somraj Mukherjee <somraj.mukherjee@yocket.in>
2023-07-09 23:46:29 +0530

    👌 IMPROVE: added heading

--- src/components/FormHeader.tsx
+++ src/components/FormHeader.tsx
@@ -6,10 +6,10 @@ interface FormHeaderProps {
 const FormHeader: React.FC<FormHeaderProps> = ({ title, description }) => {
   return (
     <div className="flex flex-col gap-2 px-4 ">
-      <h1 className="font-primary-bold text-2xl font-bold text-marine-blue">{title}</h1>
-      <h3 className="font-primary-medium text-lg font-medium text-light-gray">
+      <p className="font-primary-bold text-2xl font-bold text-marine-blue">{title}</p>
+      <p className="font-primary-medium text-lg font-medium text-light-gray">
         {description}
-      </h3>
+      </p>
     </div>
   );
 };
